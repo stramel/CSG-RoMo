a09188a674c082f0a31d67f4002785e2a02dedd7
Doug McGeehan <doug.mcgeehan@mst.edu>
2012-03-03 17:17:11 -0600

    When an individual is created with a maximum number of rules, the vector of rules will be automatically created

--- src/edu/mst/cs206/sp2012/Individual.java
+++ src/edu/mst/cs206/sp2012/Individual.java
@@ -1,14 +1,21 @@
 package edu.mst.cs206.sp2012;
 
+import java.util.Random;
 import java.util.Vector;
 
 public class Individual {
 
 	private int maxNumberOfRules;
-	private Vector ruleslist = new Vector();
+	private Vector<Rule> ruleslist = new Vector<Rule>();
 	
 	public Individual(int maxNumberOfRulesPerSolution) {
 		this.maxNumberOfRules = maxNumberOfRulesPerSolution;
+		
+		int numberOfRulesToBeCreated = new Random().nextInt(maxNumberOfRulesPerSolution);
+		for (int i=0; i<numberOfRulesToBeCreated; i++)
+		{
+			this.ruleslist.add(new Rule());
+		}
 	}
 	
 	/*private Element ProposedSummary;
