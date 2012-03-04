7f1872b0a8e30dfda8415bc81f1743bd90440aa5
Doug McGeehan <doug.mcgeehan@mst.edu>
2012-03-04 16:05:39 -0600

    Individual now initializes a random list of Rules, where the metric and the threshold are set to random values.

--- src/edu/mst/cs206/sp2012/Individual.java
+++ src/edu/mst/cs206/sp2012/Individual.java
@@ -10,6 +10,8 @@ public class Individual {
 	//private Element GeneratedSummaryTable;
 	//private OpenSourceProject ProposedSummary;
 	private Vector<Rule> ruleslist = new Vector<Rule>();
+
+	private String[] availableMetrics;
 	
 	public Individual(int maxNumberOfRulesPerSolution) {
 		this.maxNumberOfRules = maxNumberOfRulesPerSolution;
@@ -17,8 +19,16 @@ public class Individual {
 		int numberOfRulesToBeCreated = new Random().nextInt(maxNumberOfRulesPerSolution);
 		for (int i=0; i<numberOfRulesToBeCreated; i++)
 		{
-			this.ruleslist.add(new Rule());
+			Random randomizer = new Random();
+			int randomThreshold = randomizer.nextInt();
+			String randomMetric = availableMetrics[randomizer.nextInt(availableMetrics.length)];
+			this.ruleslist.add(new Rule(randomThreshold, randomMetric, null));
 		}
+	}
+
+	public void setAvailableMetrics(String[] availableMetrics) {
+		// TODO Auto-generated method stub
+		this.availableMetrics = availableMetrics;
 	}
 	
 	/*private Element ProposedSummary;
