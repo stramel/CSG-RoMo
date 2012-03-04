4a91a2c39c29eb75c611bdc1a608c64a0c08ffcf
Doug McGeehan <doug.mcgeehan@mst.edu>
2012-03-04 16:08:45 -0600

    OpenSourceProject code fix. Instead of an array of elements, we are now dealing with a Vector of elements

--- src/edu/mst/cs206/sp2012/OpenSourceProject.java
+++ src/edu/mst/cs206/sp2012/OpenSourceProject.java
@@ -3,7 +3,7 @@ package edu.mst.cs206.sp2012;
 import java.util.Vector;
 
 public class OpenSourceProject {
-	private Element[] elements;
+	private Vector<Element> elements;
 	private Summary summary;
 	
 	public OpenSourceProject(String xml, String summary) {
@@ -19,7 +19,7 @@ public class OpenSourceProject {
 		
 	}
 	
-	public Vector getElements() {
+	public Vector<Element> getElements() {
 		return elements;
 	}
 	
