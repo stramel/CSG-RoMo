ee2768a3846a7152cee859b581279b78bdebe2ec
Doug McGeehan <djmvfb@mst.edu>
2012-03-10 17:52:52 -0600

    Added new buttons for file navigation for locating the Sample Summary and the Metrics Results.

--- src/edu/mst/cs206/sp2012/GUI.java
+++ src/edu/mst/cs206/sp2012/GUI.java
@@ -3,8 +3,12 @@ package edu.mst.cs206.sp2012;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -25,6 +29,26 @@ public class GUI extends JFrame implements ActionListener {
 		GUI gui = new GUI();
 	    gui.setVisible(true);
 	}
+	
+	public class FileSystemNavigator extends AbstractAction {
+	    JFrame frame;
+	    JFileChooser chooser;
+	    JTextField textField;
+
+	    FileSystemNavigator(JTextField textField) {
+	        this.chooser = new JFileChooser();
+	        this.frame = new JFrame();
+	        this.textField = textField;
+	    }
+
+	    public void actionPerformed(ActionEvent evt) {
+	        // Show dialog; this method does not return until dialog is closed
+	        chooser.showOpenDialog(frame);
+
+	        // Get the selected file
+	        textField.setText(chooser.getSelectedFile().getAbsolutePath());
+	    }
+	};
 	
 	public GUI()
 	{
@@ -48,12 +72,21 @@ public class GUI extends JFrame implements ActionListener {
 	    inputPanel.add(numberOfRulesPerSolution);
 	    
 		pathToSampleSummary = new JTextField(20);
-		inputPanel.add(new JLabel("Path to Sample Summary: "));
-	    inputPanel.add(pathToSampleSummary);
+		JButton browseForSampleSummary = new JButton("Browse...");
+	    browseForSampleSummary.setAction(new FileSystemNavigator(pathToSampleSummary));
 	    
+	    inputPanel.add(new JLabel("Path to Sample Summary: "));
+	    inputPanel.add(pathToSampleSummary);
+	    inputPanel.add(browseForSampleSummary);
+	    
+	    JLabel labelForMetricsResult = new JLabel("Path to Metrics Results: ");
 	    pathToMetricsResults = new JTextField(20);
-		inputPanel.add(new JLabel("Path to Metrics Results: "));
+	    JButton browseForMetricsResults = new JButton("Browse...");
+	    browseForMetricsResults.setAction(new FileSystemNavigator(pathToMetricsResults));
+	    
+		inputPanel.add(labelForMetricsResult);
 	    inputPanel.add(pathToMetricsResults);
+	    inputPanel.add(browseForMetricsResults);
 	    
 	    JButton runButton = new JButton ("Run");
 	    runButton.addActionListener(this);
@@ -64,9 +97,9 @@ public class GUI extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		final int NUM_ITERATIONS = Integer.parseInt(numberOfIterations.getText());
-		final int NUM_FINAL_SOLUTIONS = Integer.parseInt(numberOfFinalSolutions.getText());
-		final int NUM_RULES_PER_SOLUTIONS = Integer.parseInt(numberOfRulesPerSolution.getText());
+		final int NUM_ITERATIONS = getNumberOfIterations();
+		final int NUM_FINAL_SOLUTIONS = getNumberOfFinalSolutions();
+		final int NUM_RULES_PER_SOLUTIONS = getNumberOfRulesPerSolution();
 		final String SAMPLE_SUMMARY_PATH = pathToSampleSummary.getText();
 		JOptionPane.showMessageDialog(this, SAMPLE_SUMMARY_PATH,
 				"Path to Sample Summary", JOptionPane.INFORMATION_MESSAGE);
@@ -78,5 +111,17 @@ public class GUI extends JFrame implements ActionListener {
 		new MainController(NUM_ITERATIONS, NUM_FINAL_SOLUTIONS,
 				NUM_RULES_PER_SOLUTIONS, SAMPLE_SUMMARY_PATH,
 				METRICS_RESULT_PATH).run();
+	}
+
+	private int getNumberOfRulesPerSolution() throws NumberFormatException {
+		return Integer.parseInt(numberOfRulesPerSolution.getText());
+	}
+
+	private int getNumberOfFinalSolutions() throws NumberFormatException {
+		return Integer.parseInt(numberOfFinalSolutions.getText());
+	}
+
+	private int getNumberOfIterations() throws NumberFormatException {
+		return Integer.parseInt(numberOfIterations.getText());
 	}
 }
