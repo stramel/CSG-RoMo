5d7c75b68561bd2bdcf839a4f1ab0ab77559e693
Doug McGeehan <djmvfb@mst.edu>
2012-03-22 18:46:45 -0500

    Added JavaDocs comments to the MainController.java file

--- src/edu/mst/cs206/sp2012/MainController.java
+++ src/edu/mst/cs206/sp2012/MainController.java
@@ -2,6 +2,7 @@ package edu.mst.cs206.sp2012;
 
 import javax.naming.InvalidNameException;
 
+/**This class is our core library, operating as a service to be called by any given user interface*/
 public class MainController {
 	private final int numberOfIterations;
 	private final int maxNumberOfSolutions;
@@ -11,6 +12,19 @@ public class MainController {
 	private Population bestSolutions;
 	private String[] availableMetrics;
 	
+	/**public MainController(int numberOfIterations, int numberOfFinalSolutions, int maxNumberOfRulesPerSolution, String urlToSampleSummaries, String urlToMetricsResults)
+	 * 
+	 * This constructor will initialize the MainController with the appropriate parameters, as specified by the initialization list.
+	 * 
+	 * availableMetrics is currently a hard-coded list of metric IDs that will be utilized by this project.
+	 * 
+	 * 
+	 * @param numberOfIterations of type int
+	 * @param numberOfFinalSolutions of type int
+	 * @param maxNumberOfRulesPerSolution of type int
+	 * @param urlToSampleSummaries of type String
+	 * @param urlToMetricsResults of type String
+	 */
 	public MainController(int numberOfIterations, int numberOfFinalSolutions,
 			int maxNumberOfRulesPerSolution, String urlToSampleSummaries,
 			String urlToMetricsResults)
@@ -31,7 +45,18 @@ public class MainController {
 		this.availableMetrics[4] = "NSC";	// Number of Children
 		this.availableMetrics[5] = "NOM";	// Number of Methods
 	}
-
+	
+	/** public void run()
+	 * This function will begin the search for a set of rules that will create a summary table.
+	 * 
+	 * The population is first initialized with the appropriate parameters (maxNumberOfSolutions, maxNumberOfRulesPerSolution, and availableMetrics).
+	 * 
+	 * The sample project information is initialized as well, and passed to the population.
+	 * 
+	 * Looping through all iterations, all unevaluated individuals in the population are evaluated. 
+	 * The best solution in the population is then stored in the Main Controller's population of
+	 * bestSolutions. The population is purged and renewed for the next iteration.
+	 */
 	public void run() throws InvalidNameException {		
 		Population currentPopulation = new Population(maxNumberOfSolutions, maxNumberOfRulesPerSolution, availableMetrics);
 		currentPopulation.setSampleProject(new OpenSourceProject(urlToMetricsResults, urlToSampleSummary, availableMetrics));
@@ -44,6 +69,9 @@ public class MainController {
 		}
 	}
 	
+	/** public Individual getBestSolution()
+	 * This function will grab the best solution of the Main Controller's population of best solutions.
+	 */
 	public Individual getBestSolution() {
 		// Output the best solutions that are stored in ctrl.bestSolutions;
 		return bestSolutions.getBestSolution();
