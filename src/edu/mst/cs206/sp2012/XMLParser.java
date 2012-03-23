579c0e7e2250656f011a556356924423740a69c3
Nathan Barron <nebt33@mst.edu>
2012-03-22 20:02:20 -0500

    javadocs for XMParser

--- src/edu/mst/cs206/sp2012/XMLParser.java
+++ src/edu/mst/cs206/sp2012/XMLParser.java
@@ -10,7 +10,15 @@ import org.w3c.dom.Element;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Vector;
-
+/**public class XMLParser
+ * This class will Parse through an XML to grab the values created by the metrics.
+ * @author nebt33
+ *
+ */
+/**public class XMlParser
+ * This function will confirm the className, save the class into the elements, save the methods of this class into elements or 
+ * throw an exception if any of these fail.  
+ */
 public class XMLParser {
 	public static Vector<edu.mst.cs206.sp2012.Element> parseMetricsXML(String filename, String[] metricsToSave) throws InvalidNameException {
 		Vector<edu.mst.cs206.sp2012.Element> elements = new Vector<edu.mst.cs206.sp2012.Element>();
@@ -54,7 +62,13 @@ public class XMLParser {
 		return elements;
 	}
 
-	
+	/**public static Summary parseSummaryXML(String filename) throws InvalidNameExcpetion
+	 * This function will grab the classes needed and add them to the summary and grab the methods needed and add them to the summary
+	 * or throw an exception.  
+	 * @param filename of type String
+	 * @return new Summary(elements)
+	 * @throws InvalidNameException
+	 */
 	public static Summary parseSummaryXML(String filename) throws InvalidNameException {
 		Vector<edu.mst.cs206.sp2012.Element> elements = new Vector<edu.mst.cs206.sp2012.Element>();
 		
@@ -94,7 +108,12 @@ public class XMLParser {
 		return new Summary(elements);
 	}
 	
-	
+	/**private static HashMap<String, Integer> getElementMetrics(Node element, String[] metricsToSave)
+	 * This function will find the metrics to specifically implement in this project and save it into a HashMap.  
+	 * @param element is a HashMap where the key is String and the value being stored is Integers.
+	 * @param metricsToSave of type String[]
+	 * @return metrics
+	 */
 	private static HashMap<String, Integer> getElementMetrics(Node element, String[] metricsToSave)
 	{
 		Node metricsNode = getFirstChildByTagName("Metrics", element);
@@ -131,12 +150,22 @@ public class XMLParser {
 		return metrics;
 	}
 	
-	// TODO Delete this block if not needed!
+	/**private static String getNodeValue(Node node)
+	 * This function will return the value of the node.
+	 * @param node of type Node
+	 * @return node.getChildNodes().item(0).getNodeValue()
+	 */
 	private static String getNodeValue(Node node)
 	{
 		return node.getChildNodes().item(0).getNodeValue();
 	}
 	
+	/**private static Node getFirstChildByTagName(String tag, Node element)
+	 * 
+	 * @param tag of type String
+	 * @param element of type element
+	 * @return null or element of type element if nodeName = tag 
+	 */
 	private static Node getFirstChildByTagName(String tag, Node element)
 	{
 		element = element.getFirstChild();
@@ -152,6 +181,12 @@ public class XMLParser {
 		return null;
 	}
 	
+	/**private static Node getNextSiblingByTagName(String tag, Node element)
+	 * 
+	 * @param tag of type String
+	 * @param element of type element
+	 * @return null or element of type element if nodeName = tag 
+	 */
 	private static Node getNextSiblingByTagName(String tag, Node element)
 	{
 		do {
