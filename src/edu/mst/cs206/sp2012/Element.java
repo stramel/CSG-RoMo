1de97eadfd9779373865dc03e88a9a3fb9dae08c
wviers <treyviers@gmail.com>
2012-03-22 19:12:19 -0500

    More Documentation

--- src/edu/mst/cs206/sp2012/Element.java
+++ src/edu/mst/cs206/sp2012/Element.java
@@ -9,7 +9,7 @@ public class Element {
 	private Type type;
 	private HashMap<String, Integer> metrics;
 
-	/**public Element(String name, Type type, HashMap<String,Integer> metrics
+	/**public Element(String name, Type type, HashMap<String,Integer> metrics)
 	 * This constructor will pass in a name, type, and metrics setting them equal to each other in this class.
 	 * @param name of type String
 	 * @param type of type Type
